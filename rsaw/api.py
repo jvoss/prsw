6ec2fd80bdc4532d7eea7b257c559b5f31bf32aa
Jonathan P. Voss <jvoss@onvox.net>
2021-04-10 16:05:46 -0500

    added very basic error checking

--- rsaw/api.py
+++ rsaw/api.py
@@ -5,6 +5,15 @@ from typing import Optional
 
 API_URL = "https://stat.ripe.net/data"
 
+
+class RequestError(Exception):
+    pass
+
+
+class ResponseError(Exception):
+    pass
+
+
 class Output:
     def __init__(
         self,
@@ -22,24 +31,31 @@ class Output:
         status_code: Optional[int] = 0,
         time: Optional[str] = "",
     ):
-        self.messages = messages
-        self.see_also = see_also
-        self.version = str(version)
-        self.data_call_status = str(data_call_status)
-        self.cached = bool(cached)
-        self.data = data
-        self.query_id = str(query_id)
-        self.process_time = int(process_time)
-        self.server_id = str(server_id)
-        self.build_version = str(build_version)
-        self.status = str(status)
-        self.status_code = int(status_code)
-        self.time = datetime.datetime.fromisoformat(time)
+
+        if status_code is 200:
+            self.messages = messages
+            self.see_also = see_also
+            self.version = str(version)
+            self.data_call_status = str(data_call_status)
+            self.cached = bool(cached)
+            self.data = data
+            self.query_id = str(query_id)
+            self.process_time = int(process_time)
+            self.server_id = str(server_id)
+            self.build_version = str(build_version)
+            self.status = str(status)
+            self.status_code = int(status_code)
+            self.time = datetime.datetime.fromisoformat(time)
+        else:
+            raise ResponseError("Invalid response from API")
 
 
 def get(path, params):
     url = API_URL + str(path) + "data.json?" + str(params)
-    r = requests.get(url)
 
-    return Output(**r.json())
-
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return Output(**response.json())
+    except Exception as e:
+        raise RequestError(e)
