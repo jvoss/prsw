0dc4e8f1d1da88fef7d1ffbb1da9cc2f5accc4d3
Jonathan P. Voss <jvoss@onvox.net>
2021-04-12 10:34:27 -0500

    gh updates

--- rsaw/__init__.py
+++ rsaw/__init__.py
@@ -2,4 +2,4 @@ from .announced_prefixes import AnnouncedPrefixes as announced_prefixes
 from .looking_glass import LookingGlass as looking_glass
 from .rpki_validation_status import RPKIValidationStatus as rpki_validation_status
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
