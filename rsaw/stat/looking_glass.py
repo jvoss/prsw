d957d0a4b99fa1efddcf18830bb9349c236c634e
Jonathan P. Voss <jvoss@onvox.net>
2021-04-13 23:45:56 -0500

    linted

--- rsaw/stat/looking_glass.py
+++ rsaw/stat/looking_glass.py
@@ -157,7 +157,7 @@ class LookingGlass:
                 peers.append(Peer(**peer))
 
             rrc["peers"] = peers
-            rrcs[rrc["rrc"]] = (RRC(**rrc))
+            rrcs[rrc["rrc"]] = RRC(**rrc)
 
         return rrcs
 
