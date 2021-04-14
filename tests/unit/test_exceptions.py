e85fb12f3ae6d664dc753cc5b5f5335362437939
Jonathan P. Voss <jvoss@onvox.net>
2021-04-14 11:24:17 -0500

    updated exception testing

--- tests/unit/test_exceptions.py
+++ tests/unit/test_exceptions.py
@@ -7,12 +7,27 @@ class TestRSAWException:
     def test_inheritance(self):
         assert issubclass(RSAWException, Exception)
 
+    def test_catch(self):
+        exception = RSAWException("test message")
+        with pytest.raises(RSAWException):
+            raise exception
+
 
 class TestRequestError:
     def test_inheritance(self):
         assert issubclass(RequestError, RSAWException)
 
+    def test_catch(self):
+        exception = RequestError("test message")
+        with pytest.raises(RequestError):
+            raise exception
+
 
 class TestResponseError:
     def test_inheritance(self):
         assert issubclass(ResponseError, RSAWException)
+
+    def test_catch(self):
+        exception = ResponseError("test message")
+        with pytest.raises(ResponseError):
+            raise exception
