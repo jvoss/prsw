6b793e0b554aa5eed36a49e448926b27d85cc9b6
Jonathan P. Voss <jvoss@onvox.net>
2021-04-14 21:16:41 -0500

    linted

--- rsaw/api.py
+++ rsaw/api.py
@@ -51,10 +51,11 @@ class Output:
             raise ResponseError("Invalid response from API")
 
 
-def get(path, params={}):
+def get(path, params=None):
     """Retrieve the requested path with parameters as GET from the API."""
+    params = {} if params is None else params
+    params = "&".join("{}={}".format(k, v) for k, v in params.items())
 
-    params = '&'.join('{}={}'.format(k,v) for k,v in params.items())
     url = API_URL + str(path) + "data.json?" + str(params)
 
     try:
